b55e7059177ae3df1226c52be09d82df56dd034c
Manish Tuladhar <manish.eclipse@gmail.com>
2020-08-11 13:46:08 +0545

    Initial code

---
+++ app/src/main/java/np/com/manishtuladhar/androidcharacter/data/AndroidImageAssets.java
@@ -0,0 +1,9 @@
+package np.com.manishtuladhar.androidcharacter.data;
+
+public class AndroidImageAssets {
+
+    //list of all android images
+
+    //heads
+
+}
---
+++ app/src/main/java/np/com/manishtuladhar/androidcharacter/ui/AndroidCharacterActivity.java
@@ -0,0 +1,16 @@
+package np.com.manishtuladhar.androidcharacter.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import np.com.manishtuladhar.androidcharacter.R;
+
+public class AndroidCharacterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_character);
+    }
+}
